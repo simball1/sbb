4fe31256017c1c8ceda5286cdbed96a5ce7fd2c1
simball1 <simball1@kakao.com>
2022-09-20 14:15:43 +0900

    회원가입 기능 추가 - 스프링 시큐리티 사용

---
+++ src/main/java/com/mysite/sbb/user/UserRepository.java
@@ -0,0 +1,7 @@
+package com.mysite.sbb.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<SiteUser, Long> {
+
+}
---
+++ src/main/java/com/mysite/sbb/user/UserService.java
@@ -0,0 +1,23 @@
+package com.mysite.sbb.user;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+	
+	public SiteUser create(String username, String email, String password) {
+		SiteUser siteUser = new SiteUser();
+		siteUser.setUsername(username);
+		siteUser.setEmail(email);
+		siteUser.setPassword(passwordEncoder.encode(password));
+		this.userRepository.save(siteUser);
+		return siteUser;
+	}
+}
